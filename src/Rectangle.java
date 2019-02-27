da05348f01df51f3917e39b747b302bb061ce248
ErinHarrington <erin.c.harrington-1@ou.edu>
2019-02-26 19:16:58 -0600

    First commit with base code

---
+++ src/Rectangle.java
@@ -0,0 +1,22 @@
+
+public class Rectangle extends Shape {
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getShapeType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
